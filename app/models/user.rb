601c9ac52035043e8b0a0a9ddc7df84436a1e6d6
Gabriel Mattos <gabrielmattos2003@hotmail.com>
2022-03-02 17:13:50 -0300

    user model done

---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ApplicationRecord
+  validates :name, :email, :password, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_03_02_200405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "users_id"
+    t.bigint "computers_id"
+    t.boolean "available", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["computers_id"], name: "index_bookings_on_computers_id"
+    t.index ["users_id"], name: "index_bookings_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.text "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
