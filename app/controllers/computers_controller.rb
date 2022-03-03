f463e2fb00163134e7d4b815c62c13e3d5a1b6e3
Gabriel Mattos <gabrielmattos2003@hotmail.com>
2022-03-03 12:32:09 -0300

    Finished controller and created the view files

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -8,18 +8,32 @@ class ComputersController < ApplicationController
   end
 
   def new
+    @computer = Computer.new
   end
 
   def create
+    @computer = Computer.new(computer_params)
+    @computer.save
   end
 
-
   def edit
+    @computer = Computer.find(params[:id])
   end
 
   def update
+    @computer = Computer.find(params[:id])
+    @computer.update(computer_params)
   end
 
   def destroy
+    @computer = Computer.find(params[:id])
+    @computer.destroy
+    redirect_to computers
+  end
+
+  private
+
+  def computer_params
+    params.require(:computers).permit(:start_date, :end_time, :available, :total_price)
   end
 end
