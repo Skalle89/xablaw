73ddef19756985638793fe07dfefbc386881e9b2
Eric Vianna <ericmvianna@gmail.com>
2022-03-03 14:35:35 -0300

    changes to devise and columns first name and last name to users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :computers
+  skip_before_action :authenticate_user!, only: :users
 
   def home; end
 end
