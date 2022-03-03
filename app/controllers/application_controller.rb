84643a40884c7a4f5b1cb5dc615828bd8c4946f7
Eric Vianna <ericmvianna@gmail.com>
2022-03-03 14:42:05 -0300

    devises and added first and last name to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_paramters, if: :devise_controller?
 
   def configure_permitted_paramters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
 end
