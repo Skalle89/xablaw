d521ce1f2cf73435c2569abc8fe6188d9b83aa16
Gabriel Mattos <gabrielmattos2003@hotmail.com>
2022-03-03 16:28:46 -0300

    Fixed.

--- db/migrate/20220303135535_create_bookings.rb
+++ db/migrate/20220303135535_create_bookings.rb
@@ -3,8 +3,8 @@ class CreateBookings < ActiveRecord::Migration[6.1]
     create_table :bookings do |t|
       t.date :start_date
       t.date :end_time
-      t.references :users, null: false, foreign_key: true
-      t.references :computers, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :computer, null: false, foreign_key: true
       t.boolean :available
       t.integer :total_price
 
