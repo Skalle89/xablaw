752fd20809a318de8e56f5616b06e542af2e7a5f
Gabriel Mattos <gabrielmattos2003@hotmail.com>
2022-03-04 16:21:36 -0300

    Edit- Done, Pundit- Done, Authorizations Rewritten

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit
   before_action :configure_permitted_paramters, if: :devise_controller?
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(computers_path)
+  end
 
   def configure_permitted_paramters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
