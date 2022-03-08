992c7c8ba50c90c3fa8bf9969e6287df8a0ca068
Eric Vianna <47088623+Skalle89@users.noreply.github.com>
2022-03-08 10:47:25 -0300

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def user_has_photo?(user)
-    return cl_image_tag @user.photo.key, height: 300, width: 400, crop: :fill if user.photo.attached?
+  def user_has_photo?
+    return cl_image_tag @user.photo.key, height: 300, width: 400, crop: :fill if @user.photo.attached?
 
     image_tag "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png",
               width: 70, style: "border-radius:100%"
