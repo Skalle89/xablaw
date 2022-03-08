f3062cbe1a1abec03264e8ad3c960e3b28968619
Eric Vianna <ericmvianna@gmail.com>
2022-03-08 11:52:04 -0300

    user avatar image on navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,17 @@ module ApplicationHelper
     image_tag "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png",
               width: 70, style: "border-radius:100%"
   end
+
+  def navbar_avatar_has_photo?(user)
+    if user.photo.attached?
+      return cl_image_tag user.photo.key, height: 40, width: 40, crop: :fill, gravity: :face,
+                                          class: "dropdown-toggle", id: "navbarDropdown",
+                                          data: { bs_toggle: "dropdown" }, 'aria-haspopup': true,
+                                          'aria-expanded': false, style: "border-radius:50%"
+    end
+
+    image_tag "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png",
+              class: "avatar dropdown-toggle", id: "navbarDropdown",
+              data: { bs_toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false
+  end
 end
